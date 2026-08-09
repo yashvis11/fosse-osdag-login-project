ae84cadd073fd093c76282bb9f0cb97d2be8b7fc
Yashvi Smart <yashvipsmart@gmail.com>
2026-08-10 00:00:54 +0530

    Added /me route and made jwt middleware

---
+++ custom_backend/middleware/authenticationMiddleware.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) =>{
+    if(!req.header.authorization){
+        return res.status(400).json({
+            message: "Authorization required!"
+        })
+    }
+    const userToken = req.headers.authorization.split(" ")[1];//seperate the word Bearer and the token
+    //verify if the token is the same and not altered using the signature 
+    //verifies the current token 
+    jwt.verify(userToken, process.env.JWT_SECRET, (err, decoded_data)=>{
+        if(error){//token has been tampered
+            return res.status(401).json({
+                message: "Unauthorized access"
+            })
+        }
+        req.user = decoded_data //no tampering get the user details into a new user property 
+        next();
+    });
+}
+module.exports = verifyToken;
--- custom_backend/routes/authenticationRoute.js
+++ custom_backend/routes/authenticationRoute.js
@@ -4,9 +4,11 @@ const express = require("express");
 const router = express.Router();
 
 const {registerUser, loginUser}= require("../controller/authenticationController");
+const verifyToken = require("../middleware/authenticationMiddleware");
 
 /*call registerUser when the path passed in the requestcontains 
 /register */
 router.post("/register", registerUser);
 router.post("/login", loginUser)
+router.post("/me", verifyToken, getUser)
 module.exports = router; 
