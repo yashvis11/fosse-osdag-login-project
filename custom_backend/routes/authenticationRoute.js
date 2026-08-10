1b4df29ea0bc4aad33e01b3a83efbb56078e9a25
Yashvi Smart <yashvipsmart@gmail.com>
2026-08-11 01:41:19 +0530

    Need to build up on the files by id function

--- custom_backend/routes/authenticationRoute.js
+++ custom_backend/routes/authenticationRoute.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const {registerUser, loginUser, getProfile, getFiles}= require("../controller/authenticationController");
+const {registerUser, loginUser, getProfile, getFiles, getFileId}= require("../controller/authenticationController");
 const {verifyToken} = require("../middleware/authenticationMiddleware");
 
 
@@ -13,4 +13,5 @@ router.post("/register", registerUser);
 router.post("/login", loginUser)
 router.get("/me", verifyToken, getProfile)
 router.get("/files", verifyToken, getFiles)
+router.get("/files/:id", verifyToken, getFileId)
 module.exports = router; 
