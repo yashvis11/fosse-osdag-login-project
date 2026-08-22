e70f334ab108dfa55465dfd26c84ec46658018d6
Yashvi Smart <yashvipsmart@gmail.com>
2026-08-22 18:18:54 +0530

    Fixed imports in index.html

--- appwrite_backend/lib/appwrite.js
+++ appwrite_backend/lib/appwrite.js
@@ -1,22 +1,8 @@
-// import { Client, Account, Databases, Storage} from "appwrite";
-const { Client, Account, Databases, Storage} = window.Appwrite
-// console.log(document.getElementById("awProjectId").value)
-// const client = new Client()
-//   .setEndpoint(document.getElementById("awEndpoint").value)
-//   .setProject(document.getElementById("awProjectId").value);
-
-// const account = new Account(client);
-// const databases = new Databases(client);
-// const storage = new Storage(client);
-
-// export { client, account, databases, storage };
-
-//initially null as these values will be filled later
-
-//as the values need to be read from the GUI this function will set the config when the backend mode
-//is changed and not when the DOM is loaded
 let appwriteMethods = null
 const configure = (endpoint, projectId) =>{
+
+  const {Client, Account, Databases, Storage} = window.Appwrite
+
   const client = new Client().setEndpoint(endpoint)
     .setProject(projectId)
 
